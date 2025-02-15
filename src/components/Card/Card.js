e00406522a35f77fb06fec6e2a7a84fa0413123c
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2025-02-15 23:52:51 +0100

    sec task done

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,11 @@
 import styles from './Card.module.scss';
+
+
 const Card = props => {
     return (
+        //<li className={styles.card}>{props.title}</li>
+        <li className={styles.card}>{props.title}<button onClick={() => props.removeCard(props.columnId, props.id)}>X</button></li>
 
-        <li className={styles.card}>{props.title}<button>X</button></li>
     );
 };
 export default Card;
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,9 +1,27 @@
-/*import Button from '../Button/Button';
+import Button from '../Button/Button';
 import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
 
 const ColumnForm = props => {
 
-    
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = event => {
+        event.preventDefault();
+        setTitle('');
+        setIcon('');
+    }
+
+    return (
+
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            Title: <input type="text" value={title} onChange={event => setTitle(event.target.value)} />
+            Icon: <input type="text" value={icon} onChange={event => setIcon(event.target.value)} />
+            <Button>Add Column</Button>
+        </form>
+    );
+};
+
+
 
 export default ColumnForm;
-*/
